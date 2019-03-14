3953b5417b98cdd23414eeddadbfd6871b210425
Diego Oliveira <diegosetubal10@hotmail.com>
2019-03-14 17:10:41 -0300

    Updating references to implement later

---
+++ entities/empreendimentos.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "id": "0957abc7-fa3f-e411-8efb-009c02b0cb58",
+    "nome": "PARQUE SERRA AZUL",
+    "nomereduzido": "PARQUE SERRA AZUL",
+    "tipo": 3,
+    "endereco": {
+        "idcidade": "6774b121-a4d9-dc11-abcf-001b7845b4a6",
+        "nomecidade": "Sorocaba",
+        "estado": "SP",
+        "bairro": "CAGUAÇU"
+    }
+}
--- src/operators.js
+++ src/operators.js
@@ -1,22 +1,24 @@
+const { map, filter, forkJoin, of, range, merge, concat } = require('rxjs');
+const { mapTo } = require('rxjs/operators');
+const empreendimento = require('../entities/empreendimentos');
+const blocos = require('../entities/blocos');
+
+let emp1 = {
+    id: empreendimento.id,
+    nome: empreendimento.nome
+};
+
+let emp2 = {
+    tipo: empreendimento.tipo,
+    nomereduzido: empreendimento.nomereduzido,
+    endereco: empreendimento.endereco
+};
 
 
-(async () => {
-    console.time("Teste de chamada de 1000ms")
-    setTimeout(() => {
-        console.log("Chamada de 1000ms")
-    }, 1000);
-    console.timeEnd("Teste de chamada de 1000ms");
-})();
-
-// (async () => {
-//     console.time("Teste de chamada de 500ms")
-//     asyncFunction(500, () => console.log("Chamada de 500ms"));
-//     console.timeEnd("Teste de chamada de 500ms");
-// })();
 
 
+const observable = merge([emp1, emp2]);
 
-
-
+observable.subscribe((x) => console.log(x));
 
 
